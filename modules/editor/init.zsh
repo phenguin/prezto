b6a21d094814b2927a45cfe907bd1b351aacd2f5
Phenguin <jcullen@mit.edu>
2014-04-09 11:36:10 -0400

    Remove annoying thing throwing error

--- modules/editor/init.zsh
+++ modules/editor/init.zsh
@@ -28,42 +28,40 @@ WORDCHARS='*?_-.[]~&;!#$%^(){}<>'
 zmodload zsh/terminfo
 typeset -gA key_info
 key_info=(
-  'Control'      '\C-'
-  'ControlLeft'  '\e[1;5D \e[5D \e\e[D \eOd'
-  'ControlRight' '\e[1;5C \e[5C \e\e[C \eOc'
-  'Escape'       '\e'
-  'Meta'         '\M-'
-  'Backspace'    "^?"
-  'Delete'       "^[[3~"
-  'F1'           "$terminfo[kf1]"
-  'F2'           "$terminfo[kf2]"
-  'F3'           "$terminfo[kf3]"
-  'F4'           "$terminfo[kf4]"
-  'F5'           "$terminfo[kf5]"
-  'F6'           "$terminfo[kf6]"
-  'F7'           "$terminfo[kf7]"
-  'F8'           "$terminfo[kf8]"
-  'F9'           "$terminfo[kf9]"
-  'F10'          "$terminfo[kf10]"
-  'F11'          "$terminfo[kf11]"
-  'F12'          "$terminfo[kf12]"
-  'Insert'       "$terminfo[kich1]"
-  'Home'         "$terminfo[khome]"
-  'PageUp'       "$terminfo[kpp]"
-  'End'          "$terminfo[kend]"
-  'PageDown'     "$terminfo[knp]"
-  'Up'           "$terminfo[kcuu1]"
-  'Left'         "$terminfo[kcub1]"
-  'Down'         "$terminfo[kcud1]"
-  'Right'        "$terminfo[kcuf1]"
-  'BackTab'      "$terminfo[kcbt]"
+  'Control'   '\C-'
+  'Escape'    '\e'
+  'Meta'      '\M-'
+  'Backspace' "^?"
+  'Delete'    "^[[3~"
+  'F1'        "$terminfo[kf1]"
+  'F2'        "$terminfo[kf2]"
+  'F3'        "$terminfo[kf3]"
+  'F4'        "$terminfo[kf4]"
+  'F5'        "$terminfo[kf5]"
+  'F6'        "$terminfo[kf6]"
+  'F7'        "$terminfo[kf7]"
+  'F8'        "$terminfo[kf8]"
+  'F9'        "$terminfo[kf9]"
+  'F10'       "$terminfo[kf10]"
+  'F11'       "$terminfo[kf11]"
+  'F12'       "$terminfo[kf12]"
+  'Insert'    "$terminfo[kich1]"
+  'Home'      "$terminfo[khome]"
+  'PageUp'    "$terminfo[kpp]"
+  'End'       "$terminfo[kend]"
+  'PageDown'  "$terminfo[knp]"
+  'Up'        "$terminfo[kcuu1]"
+  'Left'      "$terminfo[kcub1]"
+  'Down'      "$terminfo[kcud1]"
+  'Right'     "$terminfo[kcuf1]"
+  'BackTab'   "$terminfo[kcbt]"
 )
 
 # Set empty $key_info values to an invalid UTF-8 sequence to induce silent
 # bindkey failure.
 for key in "${(k)key_info[@]}"; do
   if [[ -z "$key_info[$key]" ]]; then
-    key_info[$key]='�'
+    key_info["$key"]='�'
   fi
 done
 
@@ -109,39 +107,30 @@ function editor-info {
 }
 zle -N editor-info
 
-# Updates editor information when the keymap changes.
-function zle-keymap-select {
+# Ensures that $terminfo values are valid and updates editor information when
+# the keymap changes.
+function zle-keymap-select zle-line-init zle-line-finish {
+  # The terminal must be in application mode when ZLE is active for $terminfo
+  # values to be valid.
+  if (( $+terminfo[smkx] && $+terminfo[rmkx] )); then
+    case "$0" in
+      (zle-line-init)
+	# Enable terminal application mode.
+	echoti smkx
+      ;;
+      (zle-line-finish)
+	# Disable terminal application mode.
+	echoti rmkx
+      ;;
+    esac
+  fi
+
+  # Update editor information.
   zle editor-info
 }
 zle -N zle-keymap-select
-
-# Enables terminal application mode and updates editor information.
-function zle-line-init {
-  # The terminal must be in application mode when ZLE is active for $terminfo
-  # values to be valid.
-  if (( $+terminfo[smkx] )); then
-    # Enable terminal application mode.
-    echoti smkx
-  fi
-
-  # Update editor information.
-  zle editor-info
-}
-zle -N zle-line-init
-
-# Disables terminal application mode and updates editor information.
-function zle-line-finish {
-  # The terminal must be in application mode when ZLE is active for $terminfo
-  # values to be valid.
-  if (( $+terminfo[rmkx] )); then
-    # Disable terminal application mode.
-    echoti rmkx
-  fi
-
-  # Update editor information.
-  zle editor-info
-}
 zle -N zle-line-finish
+zle -N zle-line-init
 
 # Toggles emacs overwrite mode and updates editor information.
 function overwrite-mode {
@@ -208,10 +197,10 @@ bindkey -d
 # Emacs Key Bindings
 #
 
-for key in "$key_info[Escape]"{B,b} "${(s: :)key_info[ControlLeft]}"
-  bindkey -M emacs "$key" emacs-backward-word
-for key in "$key_info[Escape]"{F,f} "${(s: :)key_info[ControlRight]}"
-  bindkey -M emacs "$key" emacs-forward-word
+for key ("$key_info[Escape]"{B,b}) bindkey -M emacs "$key" emacs-backward-word
+for key ("$key_info[Escape]"{F,f}) bindkey -M emacs "$key" emacs-forward-word
+bindkey -M emacs "$key_info[Escape]$key_info[Left]" emacs-backward-word
+bindkey -M emacs "$key_info[Escape]$key_info[Right]" emacs-forward-word
 
 # Kill to the beginning of the line.
 for key in "$key_info[Escape]"{K,k}
@@ -288,10 +277,6 @@ for keymap in 'emacs' 'viins'; do
   for key in "$key_info[Control]Q" "$key_info[Escape]"{q,Q}
     bindkey -M "$keymap" "$key" push-line-or-edit
 
-  # Bind Shift + Tab to go to the previous menu item.
-  bindkey -M "$keymap" "$key_info[BackTab]" reverse-menu-complete
-
-  # Complete in the middle of word.
   bindkey -M "$keymap" "$key_info[Control]I" expand-or-complete
 
   # Expand .... to ../..
